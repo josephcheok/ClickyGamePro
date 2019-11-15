c20c05acfc0dcff27e4004675f5450705b8dde5c
josephcheok <joseph@cheokandco.com>
2019-11-15 22:45:02 +1100

    Spacing for form optimised

--- client/src/components/Formfill/Formfill.js
+++ client/src/components/Formfill/Formfill.js
@@ -2,14 +2,36 @@ import React from "react";
 import "./Formfill.css";
 
 const Formfill = props => (
-  <form class="form-inline" action="/action_page.php">
-    <label for="name">Name:</label>
-    <input type="text" id="name" placeholder="Enter Name" name="name" />
-    <label for="state">State:</label>
-    <input type="text" id="state" placeholder="" name="state" />
-    <label for="email">Email:</label>
-    <input type="email" id="email" placeholder="Enter email" name="email" />
-    <button type="submit">Submit</button>
+  <form className="form-inline" action="/action_page.php">
+    <label for="name" style={{ "margin-left": 50 }}>
+      NAME:
+    </label>
+    <input
+      type="text"
+      id="name"
+      placeholder="Enter Name"
+      name="name"
+      style={{ width: 300 }}
+    />
+    <label for="state">STATE:</label>
+    <input
+      type="text"
+      id="state"
+      placeholder=""
+      name="state"
+      style={{ width: 100 }}
+    />
+    <label for="email">EMAIL:</label>
+    <input
+      type="email"
+      id="email"
+      placeholder="Enter email"
+      name="email"
+      style={{ width: 300 }}
+    />
+    <button type="submit" style={{ "margin-right": 50 }}>
+      Submit
+    </button>
   </form>
 );
 
